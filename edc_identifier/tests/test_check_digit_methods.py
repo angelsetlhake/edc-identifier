942f33c0515e6a813b8e419fdead5cb2a190fa9c
angelsetlhake <asetlhake@yahoo.com>
2015-07-02 11:37:35 +0200

    Update test_check_digit_methods.py

--- edc_identifier/tests/test_check_digit_methods.py
+++ edc_identifier/tests/test_check_digit_methods.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 
 from ..classes import CheckDigit
-<<<<<<< HEAD
-
 
 
 class CheckDigitMethodsTests(TestCase):
@@ -37,67 +35,15 @@ class CheckDigitMethodsTests(TestCase):
         checkDigit=self.instance%modulus
         self.assertEqual(CheckDigit.calculate(self,number=self.instance,modulus=modulus),'071')
         
-=======
-from ..exceptions import CheckDigitError, IdentifierEncodingError, IdentifierDecodingError
-
-
-class CheckDigitMethodsTests(TestCase):
-
-    def test_modulus_greater_than_ten(self):
-        number=85749-3-283
-        modulus=11
-        checkDigit=number%modulus
-        self.assertEqual(CheckDigit.calculate(number,modulus),checkDigit)
-         
-
-    def test_check_num_correct_input(self):
-        number=4593
-        modulus=10
-        checkDigit=number%modulus
-        self.assertEqual(CheckDigit.calculate(number,modulus),checkDigit)  
-
-    def test_modulus_greater_than_100(self):
-        number=1237-93-876912
-        modulus=102
-        checkDigit=number%modulus
-        self.assertTrue(CheckDigit.calculate(number,modulus),checkDigit)
-        
-    
->>>>>>> 17cdb2dfd138288c50c8d002a1bb0a8523a2786b
     def test_modulus_zero(self):    
         number=7469-2-3456
         modulus=0
         self.assertRaisesRegexp(ValueError,"cannot divide by 0")  
    
-         
     def test_invalid_input(self):
-<<<<<<< HEAD
         "asserts that an exception is raised if number given is a string"
         number='9785637291 '
         modulus=7
         self.assertRaisesRegexp(ValueError,"number cannot be a string")  
-     
-    def test_check_num_wrong_input(self):
-=======
-        number='9785637291 '
-        modulus=7
-        self.assertRaisesRegexp(ValueError,"number cannot be a string")    
-        
-        
-     
-    def test_check_num_wrong_input(self):
     
->>>>>>> 17cdb2dfd138288c50c8d002a1bb0a8523a2786b
-        number=1237
-        modulus=102
-        checkDigit=number%modulus
-        self.assertNotEqual(CheckDigit.calculate(number,modulus),checkDigit)
-            
-             
-      
-                  
-<<<<<<< HEAD
-=======
-                  
->>>>>>> 17cdb2dfd138288c50c8d002a1bb0a8523a2786b
     
